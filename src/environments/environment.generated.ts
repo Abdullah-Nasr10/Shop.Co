d8a94e3c59d9b187f77e472c9983dbfdb2693ae6
Abdullah Nasr <abdullah.nasr1022@gmail.com>
2025-10-23 02:55:18 +0300

    add shopco project and server

--- src/environments/environment.generated.ts
+++ src/environments/environment.generated.ts
@@ -2,5 +2,5 @@
 // Do NOT commit this file if it contains secrets for production.
 export const environment = {
   API_BASE_URL: 'http://localhost:3000',
-  OPENAI_API_KEY: 'YOUR_OPENAI_API',
+  OPENAI_API_KEY: '',
 };
